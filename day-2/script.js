d9f980406a9de5842b6e9a0d653eea3ac08a90fe
Donatron <don.macarthur68@gmail.com>
2018-12-05 13:48:05 +0700

    solved day-2 part two challenge

--- day-2/script.js
+++ day-2/script.js
@@ -75,3 +75,50 @@ for (let k = 0; k < charMapArray.length; k++) {
 }
 
 console.log(`Result: ${doubles * triples}`);
+
+/*
+ * Part Two
+ */
+
+// Create variable to store answer
+let answer;
+
+// Create function to compare each character of a string to the character at same position of the next string
+const compareStrings = (string1, string2) => {
+  let counter = 0;
+  let matchingString = "";
+  let matchedChar;
+
+  for (let i = 0; i < string1.length; i++) {
+    // Check whether character at i is different for each string
+    // If different increment counter
+    if (string1.charAt(i) !== string2.charAt(i)) {
+      // Find index of matched character;
+      matchedChar = i;
+      counter++;
+    }
+  }
+
+  // If only one character matched set matching string to String1
+  if (counter === 1) {
+    matchingString = string1;
+    // Make substring from start of matching string to matching character
+    let substr1 = matchingString.slice(0, matchedChar);
+    // Make substring from character after matching character to the end of string
+    let substr2 = matchingString.slice(matchedChar + 1, matchingString.length);
+
+    // Concatenate substrings and submit answer
+    console.log(`Answer: ${substr1.concat(substr2)}`);
+  }
+};
+
+// Iterate over input array and compare strings
+for (let j = 0; j < input.length; j++) {
+  for (let k = 0; k < input.length; k++) {
+    if (j === k) {
+      continue;
+    } else {
+      compareStrings(input[j], input[k]);
+    }
+  }
+}
